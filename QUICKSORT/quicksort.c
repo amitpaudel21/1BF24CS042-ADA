794fabae7087d5a7ff72f42c778ada01aac910ae
Amit Paudel <amitpaudel.cs24@bmsce.ac.in>
2026-03-23 07:45:25 +0530

    Add files via upload

---
+++ QUICKSORT/quicksort.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;   /* this line was missing in your notes */
+}
+
+void quicksort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quicksort(arr, low, pi - 1);
+        quicksort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int *arr = malloc(n * sizeof(int));
+
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("\nElements before sorting: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    clock_t start = clock();
+    quicksort(arr, 0, n - 1);
+    clock_t end = clock();
+
+    printf("\nElements after sorting:  ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    double time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("\n\nTime taken for Quick sort: %f seconds\n", time_taken);
+
+    free(arr);
+    return 0;
+}
